490edbb8b2b1a6058fa3081fc03687fe9a85769e
rab87barnes <rab87barnes@aol.com>
2020-07-14 23:36:21 -0400

    hw push3

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(".changeBTN1").click(function(){
+    console.log("click is working Btn 1");
+    
+    $(".firstDiv").addClass("firstDivnew")
+})
+
+$(".changeBTN2").click(function(){
+    console.log("click is working Btn 2");
+
+    $(".secondDiv").css("background","yellow")
+})
+
+$(".changeBTN3").click(function(){
+    console.log("click is working Btn 3")
+})
+
+$(".changeAllBTN").click(function(){
+    console.log("click is working Change All")
+})
